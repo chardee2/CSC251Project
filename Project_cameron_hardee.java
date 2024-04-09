2e236e959613cbeec8b6bf5af72932c35fcd83e0
chardee2 <chardee2@email.cpcc.edu>
2024-04-08 23:59:00 -0400

    Update Project_cameron_hardee.java
    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_cameron_hardee.java
+++ Project_cameron_hardee.java
@@ -5,10 +5,10 @@ public class Project_cameron_hardee {
     
     public static void main(String[] args) throws IOException {
         //variables & arrays
-        int policyNum;
-        String providerName;
-        String firstName;
-        String lastName; 
+        int policyNum; //insurance policy number
+        String providerName; //name of insurance provider
+        String firstName; //customer's first name
+        String lastName; //customer's last name
         int age; //customer's age
         String smokingStatus; //customer's smoking status (smoking/non-smoking)
         int height; //customer's height in inches
@@ -18,7 +18,6 @@ public class Project_cameron_hardee {
 
         
         //objects
-        Scanner input = new Scanner(System.in);
         File policyInfoFile = new File("PolicyInformation.txt");  //creates file object for our policy information file
         Scanner inputFile = new Scanner(policyInfoFile); //create an object from scanner than can read the policy information file
         var policies = new ArrayList<Policy>(); //create an array list container for Policy objects
@@ -33,15 +32,18 @@ public class Project_cameron_hardee {
         //While loop to read the text file lines and set local variables equal to them. While loop then creates an object and stores it in the ArrayList container for each object
         while(inputFile.hasNext())
         {
-            policyNum = inputFile.nextInt(); 
+            policyNum = inputFile.nextInt();
+            inputFile.nextLine();
             providerName = inputFile.nextLine();
             firstName = inputFile.nextLine();
             lastName = inputFile.nextLine();
             age = inputFile.nextInt();
+            
             smokingStatus = inputFile.nextLine();
+            inputFile.nextLine();
             height = inputFile.nextInt();
             weight = inputFile.nextInt();
-            
+
             //nested if statement is used in conjunction with an incrementer to count the amount of smokers and non smokers
             if (smokingStatus.equalsIgnoreCase("smoker"))
             {
@@ -59,20 +61,24 @@ public class Project_cameron_hardee {
 
         }
 
+        inputFile.close(); //close file
+        
+       for (int i = 0; i < policies.size(); i++ ) {
+        System.out.print("\nPolicy number: " + policies.get(i).getPolicyNumber());
+        System.out.print("\nProvider Name: " + policies.get(i).getProviderName());
+        System.out.print("\nPolicyholder's First Name: " + policies.get(i).getFirstName());
+        System.out.print("\nPolicyholder's Last Name: " + policies.get(i).getLastName());
+        System.out.print("\nPolicyholder's age: " + policies.get(i).getAge());
+        System.out.print("\nPolicyholder's smoking status: " + policies.get(i).getSmokingStatus());
+        System.out.printf("\nPolicyholder's height: %.1f", policies.get(i).getHeight());
+        System.out.printf("\nPolicyholder's weight: %.1f", policies.get(i).getWeight());
+        System.out.printf("\nPolicyholder's BMI: %.2f", policies.get(i).calcBMI());
+        System.out.printf("\nPolicy Price: $%.2f\n", policies.get(i).calcPolicyPrice());
        
-        /*
-        //display info
-        System.out.print("\nPolicy number: " + policy1.getPolicyNumber());
-        System.out.print("\nProvider Name: " + policy1.getProviderName());
-        System.out.print("\nPolicyholder's First Name: " + policy1.getFirstName());
-        System.out.print("\nPolicyholder's Last Name: " + policy1.getLastName());
-        System.out.print("\nPolicyholder's age: " + policy1.getAge());
-        System.out.print("\nPolicyholder's smoking status: " + policy1.getSmokingStatus());
-        System.out.printf("\nPolicyholder's height: %.1f", policy1.getHeight());
-        System.out.printf("\nPolicyholder's weight: %.1f", policy1.getWeight());
-        System.out.printf("\nPolicyholder's BMI: %.2f", policy1.calcBMI());
-        System.out.printf("\nPolicy Price: $%.2f", policy1.calcPolicyPrice());
-        */
+       }
+     
+       System.out.println("\nThe number of policies with a smoker is: " + totalSmokers);
+       System.out.println("\nThe number of policies with a non-smoker is: " + totalNonSmokers);
     }   
 
 }
